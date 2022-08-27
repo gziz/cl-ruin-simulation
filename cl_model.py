b5ff1bc45b117d74a0cc38216eab722bd1d0539b
gziz <69287011+gziz@users.noreply.github.com>
2022-08-26 23:32:15 -0500

    First commit

---
+++ cl_model.py
@@ -0,0 +1,55 @@
+import random
+import numpy as np
+
+
+class CL:
+
+    def __init__(self,config):
+        self.config = config
+        
+    def create_claim_sz(self):
+        mu = self.config.get('claim_sz_mu')
+        lambd = 1/mu
+        #return random.gammavariate(self.alpha, self.beta)
+        return random.expovariate(lambd=lambd)
+        
+    def create_claims_cnt(self):
+        lambd = self.config.get('claim_rate_lambd')
+        total_time = 0
+        claims_cnt = 0
+        
+        while total_time < 1:
+            prob = np.random.uniform()
+            time_interval = (-np.log(prob)) / lambd
+            total_time += time_interval
+            claims_cnt += 1
+
+        return claims_cnt
+
+    def compute_claims_total(self):
+        n_claims = self.create_claims_cnt()
+
+        total = 0
+        for _ in range(n_claims):
+            total += self.create_claim_sz()
+
+        return total
+
+    def simulate(self):
+        u = self.config.get('u')
+        c = self.config.get('c')
+        time_steps = self.config.get('time_steps')
+
+        X = [u]
+
+        for _ in range(time_steps):    
+            X.append(X[-1] + c - self.compute_claims_total())
+
+        return X
+
+    def generate_simulations(self, n):
+        simulations = []
+        for _ in range(n):
+            curr_sim = self.simulate()
+            simulations.append(curr_sim)
+        return simulations
---
+++ main.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+from cl_model import CL
+
+config = {
+    "claim_sz_mu": 30774,
+    "claim_rate_lambd": 74,
+    "u": 10_000_000,
+    "c": 2_309_004,
+    "time_steps": 1000
+}
+
+def generate_simulations_data(n_simulations, config_sliders):
+
+    ### UPDATE FROM SLIDERS INPUT ###
+    for key, val in config_sliders.items():
+        config[key] = val
+
+    ### GENERATE SIMULATIONS ###
+    cl = CL(config)
+    arrays = cl.generate_simulations(n_simulations)
+
+    ### ADD RUIN LINE THRESHOLD ####
+    zeros = [0]*1000
+    arrays.append(zeros)
+
+    df = pd.DataFrame(arrays).T
+    return df
+
+
+def main():
+
+    ### MAIN ###
+    st.title('Simulaciones Modelo Cramer-Lundberg')
+    st.subheader('Simulaciones')
+
+    line_chart = st.line_chart([10_000_000.0])
+
+    ### SIDE BAR ###
+    st.sidebar.title("Valores Iniciales")
+    n_simulations = st.sidebar.slider('Número de simulaciones', 1,15,1)
+    u = st.sidebar.number_input('Capital Inicial', 10_000_000)
+    c = st.sidebar.number_input('Primas Diarias', 2_309_004)
+
+    config_sliders = {'u': u, 'c':c}
+
+    ### BUTTON LOGIC ###
+    if st.button('Run'):
+        # Loading Spinner
+        with st.spinner('Simulando'):
+            df = generate_simulations_data(n_simulations, config_sliders)
+            line_chart.add_rows(df)
+
+        st.success('Simulación terminada!')
+
+    
+if __name__ == "__main__":
+    main()
