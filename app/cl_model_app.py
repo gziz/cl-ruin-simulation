0b54f8232f85a6876bc906bb596bb7337cfb5262
gziz <69287011+gziz@users.noreply.github.com>
2022-08-31 23:55:29 -0500

    Improved numpy speed

--- app/cl_model_app.py
+++ app/cl_model_app.py
@@ -2,41 +2,36 @@ import random
 import numpy as np
 
 class CL:
-    
+
     def __init__(self,config):
         self.config = config
-        
+        self.create_claim_sz = np.vectorize(self.create_claim_sz)
+
+
     # Generar un tamaño de reclamo $
     def create_claim_sz(self, size: int): #where size is n_claims
         
         shape = self.config.get('claim_sz_shape')
         scale = self.config.get('claim_sz_scale')
 
-        return np.random.gamma(shape=shape, scale=scale, size=size)
+        return - np.random.gamma(shape=shape, scale=scale, size=size).sum()
 
         
     # Generar número de reclamos por un dia
     def create_claims_cnt(self):
+        time_steps = self.config.get('time_steps')
         lambd = self.config.get('claim_rate_lambd')
-        scale = 1/lambd
-        total_time = 0
-        claims_cnt = 0
-        
-        while total_time < 1:
+        return np.random.poisson(lam=lambd, size=time_steps)
 
-            time_interval = np.random.exponential(scale=scale)
-            total_time += time_interval
-            claims_cnt += 1
-
-        return claims_cnt
 
     # Generar total($) reclamos por un dia
     def compute_claims_total(self):
-        n_claims = self.create_claims_cnt()
 
-        total = self.create_claim_sz(size=n_claims).sum()
+        array_claims = self.create_claims_cnt()
+        array_daily_total = self.create_claim_sz(array_claims)
 
-        return total
+        return array_daily_total
+
 
     # Correr una simulación
     def simulate(self):
@@ -44,19 +39,22 @@ class CL:
         c = self.config.get('c')
         time_steps = self.config.get('time_steps')
 
-        X = [u]
-        for _ in range(time_steps):    
-            X.append(X[-1] + c - self.compute_claims_total())
+        array_daily_total = self.compute_claims_total()
+        array_daily_total_cum = array_daily_total.cumsum()
 
-        return X
+        primes_cum = np.repeat(c, time_steps).cumsum()
+        array_daily_total_cum = np.add(array_daily_total_cum, primes_cum)
+        
+        array_daily_total_cum = np.add(array_daily_total_cum, u)
+        array_daily_total_cum = np.insert(array_daily_total_cum, 0, u)
 
-    # Correr n simulaciones
+        return array_daily_total_cum
+
+
     def generate_simulations(self, n):
         simulations = []
-        for _ in range(n):
+        for idx in range(n):
             curr_sim = self.simulate()
             simulations.append(curr_sim)
+
         return simulations
-
-
-        
--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ from cl_model_app import CL
 
 config = {
     "claim_sz_shape": 0.99264,
-    "claim_sz_scale": 31002.83,
-    "claim_rate_lambd": 74.2,
-    "u": 10000000,
-    "c": 2313775.54,
-    "time_steps": 1000,
+    "claim_sz_scale": 30774,
+    "claim_rate_lambd": 74,
+    "u": 10_000_000,
+    "c": 2259838.120730937,
+    "time_steps": 1000
 }
 
 def generate_simulations_data(n_simulations, config_sliders):
@@ -35,22 +35,26 @@ def main():
     st.title('Modelo Cramer-Lundberg')
     st.subheader('Simulaciones')
 
-    line_chart = st.line_chart([10_000_000.0])
+    #u = 10_000_000.0
+    chart = st.empty()
+    chart.line_chart([10_000_000.0])
 
     ### SIDE BAR ###
     st.sidebar.title("Valores Iniciales")
-    n_simulations = st.sidebar.slider('Número de simulaciones', 1,15,1)
-    u = st.sidebar.number_input('Capital Inicial', 10_000_000)
-    c = st.sidebar.number_input('Primas Diarias', 2_313_775)
-
-    config_sliders = {'u': u, 'c':c}
+    n_simulations = st.sidebar.slider('Número de simulaciones', 1,25,1)
+    u = st.sidebar.number_input('Capital Inicial', min_value=0, value=10_000_000)
+    c = st.sidebar.number_input('Primas Diarias', value=2259838)
+    time_steps = st.sidebar.number_input('Días', value=1000, min_value=1,max_value=10000)
+    
+    config_sliders = {'u': u, 'c':c, 'time_steps':time_steps}
 
     ### BUTTON LOGIC ###
     if st.button('Simular'):
         # Loading Spinner
         with st.spinner('Simulando'):
             df = generate_simulations_data(n_simulations, config_sliders)
-            line_chart.add_rows(df)
+
+            chart.line_chart(df)
 
         st.success('Simulación terminada!')
 
